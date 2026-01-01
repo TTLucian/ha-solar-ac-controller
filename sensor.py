2537153e899709dbb31e1aadfc2b00cc4589b22b
TTLucian <1855276+TTLucian@users.noreply.github.com>
2026-01-01 17:01:32 +0200

    Create sensor.py

---
+++ sensor.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN
+from .entity import SolarACDebugEntity
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+):
+    data = hass.data[DOMAIN][entry.entry_id]
+    coordinator = data["coordinator"]
+
+    async_add_entities([SolarACDebugEntity(coordinator)])
