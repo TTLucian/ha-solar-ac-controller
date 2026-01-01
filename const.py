af846ef37dd7084c07ec03bca83cebbd0d45f675
TTLucian <1855276+TTLucian@users.noreply.github.com>
2026-01-01 16:48:25 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,7 @@
+DOMAIN = "solar_ac_controller"
+
+CONF_SOLAR_SENSOR = "solar_sensor"
+CONF_GRID_SENSOR = "grid_sensor"
+CONF_AC_POWER_SENSOR = "ac_power_sensor"
+CONF_AC_SWITCH = "ac_switch"
+CONF_ZONES = "zones"
