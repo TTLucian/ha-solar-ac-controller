1ffd7a4bb2281a6fe61a305dda1ce7b2a28fef9a
TTLucian <1855276+TTLucian@users.noreply.github.com>
2026-01-01 18:23:49 +0200

    Update const.py

--- custom_components/solar_ac_controller/const.py
+++ custom_components/solar_ac_controller/const.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 DOMAIN = "solar_ac_controller"
 
 CONF_SOLAR_SENSOR = "solar_sensor"
@@ -5,8 +7,14 @@ CONF_GRID_SENSOR = "grid_sensor"
 CONF_AC_POWER_SENSOR = "ac_power_sensor"
 CONF_AC_SWITCH = "ac_switch"
 CONF_ZONES = "zones"
+
 CONF_SOLAR_THRESHOLD_ON = "solar_threshold_on"
 CONF_SOLAR_THRESHOLD_OFF = "solar_threshold_off"
 
+# New panic configuration
+CONF_PANIC_THRESHOLD = "panic_threshold"
+CONF_PANIC_DELAY = "panic_delay"
+
+# Storage
 STORAGE_KEY = DOMAIN
 STORAGE_VERSION = 1
