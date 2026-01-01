1d817f1d245db656c8082f7d1fcf2cffad1797db
TTLucian <1855276+TTLucian@users.noreply.github.com>
2026-01-01 17:22:06 +0200

    Update const.py

--- const.py
+++ const.py
@@ -5,6 +5,8 @@ CONF_GRID_SENSOR = "grid_sensor"
 CONF_AC_POWER_SENSOR = "ac_power_sensor"
 CONF_AC_SWITCH = "ac_switch"
 CONF_ZONES = "zones"
+CONF_SOLAR_THRESHOLD_ON = "solar_threshold_on"
+CONF_SOLAR_THRESHOLD_OFF = "solar_threshold_off"
 
 STORAGE_KEY = DOMAIN
 STORAGE_VERSION = 1
