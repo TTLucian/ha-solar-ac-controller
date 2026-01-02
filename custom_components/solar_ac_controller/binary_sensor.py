e6a775dc0b4d2224914b8e364a92593c0224049f
TTLucian <1855276+TTLucian@users.noreply.github.com>
2026-01-02 12:19:51 +0200

    Update binary_sensor.py

--- custom_components/solar_ac_controller/binary_sensor.py
+++ custom_components/solar_ac_controller/binary_sensor.py
@@ -28,6 +28,25 @@ async def async_setup_entry(
 
     async_add_entities(entities)
 
+class _BaseSolarACBinary(BinarySensorEntity):
+    _attr_should_poll = False
+
+    def __init__(self, coordinator):
+        self.coordinator = coordinator
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, "solar_ac_controller")},
+            "name": "Solar AC Controller",
+            "manufacturer": "TTLucian",
+            "model": "Solar AC Smart Controller",
+            "sw_version": self.coordinator.config.get("version", "0.1.3"),
+            "hw_version": "virtual",
+            "configuration_url": "https://github.com/TTLucian/ha-solar-ac-controller",
+            "suggested_area": "HVAC",
+            "entry_type": "service",
+        }
+
+    async def async_added_to_hass(self):
+        self.coordinator.async_add_listener(self.async_write_ha_state)
 
 class _BaseSolarACBinary(BinarySensorEntity):
     _attr_should_poll = False
